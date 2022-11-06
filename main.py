4f12013e2eb4fbfa477318efc5dd0b2a308611ef
Armagetto <forfxp12@gmail.com>
2022-11-06 02:06:33 +0200

    Testing of basic time logic

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ import time
 from datetime import datetime, timedelta
 
 
+def get_last_day_hour(day_range: str):
+    if day_range == '':
+        return ''
+
+    last_time = (day_range.split("-")[1]).split(":")
+
+    return int(last_time[0]) + int(last_time[1])/60
+
+
+def get_first_day_hour(day_range: str):
+    if day_range == '':
+        return ''
+
+    last_time = (day_range.split("-")[0]).split(":")
+
+    return int(last_time[0]) + int(last_time[1])/60
+
+
 class Parker:
 
-    def __init__(self, sunday, monday, tuesday, wednesday, thursday, friday):
+    def __init__(self, phone, sunday, monday, tuesday, wednesday, thursday, friday):
+        self.phone = phone
         self.sunday = sunday
         self.monday = monday
         self.tuesday = tuesday
@@ -21,15 +40,17 @@ with open("D:\\python\\random_test\\01.csv", "r", encoding="UTF-8") as csv_file:
     next(csv_reader)  # ignor the first one
 
     for line in csv_reader:
-        parker = Parker(line[3], line[4], line[5], line[6], line[7], line[8])
-        # TODO: logic loop for the compare part of days and hours
-        #see if free by day
+        # get the hours for parker
+        parker = Parker(phone=str(line[2]), sunday=str(line[3]), monday=line[4], tuesday=line[5],
+                        wednesday=line[6], thursday=line[7], friday=line[8])
+        print(parker.sunday)
+        print(get_first_day_hour(parker.sunday))
+        print(get_last_day_hour(parker.sunday))
+        # for line_to_look in csv_reader:
+        #     if int(parker.sunday.split(":")) < int(line_to_look[0][5:]):
+        #         print(parker.phone)
 
-        #free by hour
-        # sunday
-        #see if any match the same day
+        # TODO: logic loop for the compare part of days and hours
+        # go day by day and see if free for the other guys
 
         # monday
-
-
-print(parker.sunday)
